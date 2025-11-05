1375977e140ba1d72459ff39c89dda52cf97e6b4
Amey Bhutkar <130828204+ameybhutkar@users.noreply.github.com>
2025-11-05 14:09:22 +0530

    Update solution.py

--- Python Introduction/Write a fuction/solution.py
+++ Python Introduction/Write a fuction/solution.py
@@ -1,1 +1,19 @@
+def is_leap(year):
+    leap = False
+    
+    # condition for leap year 
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+            if (year % 400 == 0):
+                leap = True
+            else:
+                leap = False
+        else:
+            leap = True
+    else:
+        leap = False
+    
+    return leap
 
+year = int(input())
+print(is_leap(year))
