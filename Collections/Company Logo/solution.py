551a1dfddfc2e611a048aa9ebb2822e724830aac
Amey Bhutkar <130828204+ameybhutkar@users.noreply.github.com>
2025-12-05 11:57:50 +0530

    Create solution.py

---
+++ Collections/Company Logo/solution.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+from collections import Counter
+
+if __name__ == '__main__':
+    s = input()
+# Count characters
+count = Counter(s)
+# Sort by:
+# 1. Highest frequency (descending)
+# 2. Alphabetical order (ascending)
+result = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+
+# Print top 3
+for char, freq in result[:3]:
+    print(char, freq)
